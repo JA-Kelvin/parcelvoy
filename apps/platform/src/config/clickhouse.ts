4a7828ed462639fe8a4dd9e90773b092080f4c1d
Chris Anderson <hi@chrisanderson.io>
2025-06-09 18:53:05 -0500

    chore: move to async inserts

--- apps/platform/src/config/clickhouse.ts
+++ apps/platform/src/config/clickhouse.ts
@@ -16,6 +16,10 @@ export default (config: ClickhouseConfig) => {
             // Allows to insert serialized JS Dates (such as '2023-12-06T10:54:48.000Z')
             date_time_input_format: 'best_effort',
             output_format_json_quote_64bit_integers: 0, // Disable quoting of 64-bit integers
+            async_insert: 1,
+            wait_for_async_insert: 1,
+            async_insert_deduplicate: 1,
+            async_insert_busy_timeout_ms: 1000,
         },
     })
 }
