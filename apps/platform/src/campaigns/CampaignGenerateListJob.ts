02c452fd22a02ad68fc2f4193cfa243fd6cacde6
Chris Anderson <hi@chrisanderson.io>
2025-07-07 06:01:32 -0700

    chore: add better logging to campaign generation

--- apps/platform/src/campaigns/CampaignGenerateListJob.ts
+++ apps/platform/src/campaigns/CampaignGenerateListJob.ts
@@ -18,34 +18,41 @@ export default class CampaignGenerateListJob extends Job {
         const key = `campaign_generate_${id}`
 
         logger.info({ campaign_id: id }, 'campaign:generate:loading')
-        const campaign = await getCampaign(id, project_id) as SentCampaign
-        if (!campaign) return
-        if (campaign.state === 'aborted' || campaign.state === 'draft') return
+        try {
+            const campaign = await getCampaign(id, project_id) as SentCampaign
+            if (!campaign) return
+            if (campaign.state === 'aborted' || campaign.state === 'draft') return
 
-        // Approximate the size of the send list
-        const estimatedSize = await estimatedSendSize(campaign)
+            // Approximate the size of the send list
+            const estimatedSize = await estimatedSendSize(campaign)
 
-        // Increase lock duration based on estimated send size
-        const lockTime = Math.ceil(Math.max(estimatedSize / 500, 900))
-        logger.info({ campaignId: id, estimatedSize, lockTime }, 'campaign:generate:estimated_size')
+            // Increase lock duration based on estimated send size
+            const lockTime = Math.ceil(Math.max(estimatedSize / 500, 900))
+            logger.info({ campaignId: id, estimatedSize, lockTime }, 'campaign:generate:estimated_size')
 
-        const acquired = await acquireLock({ key, timeout: lockTime })
-        logger.info({ campaignId: id, acquired }, 'campaign:generate:lock')
-        if (!acquired) return
+            const acquired = await acquireLock({ key, timeout: lockTime })
+            logger.info({ campaignId: id, acquired }, 'campaign:generate:lock')
+            if (!acquired) return
 
-        // Use approximate size for progress
-        await cacheSet<number>(App.main.redis, CacheKeys.populationTotal(campaign), estimatedSize, 86400)
-        await cacheSet<number>(App.main.redis, CacheKeys.populationProgress(campaign), 0, 86400)
+            // Use approximate size for progress
+            await cacheSet<number>(App.main.redis, CacheKeys.populationTotal(campaign), estimatedSize, 86400)
+            await cacheSet<number>(App.main.redis, CacheKeys.populationProgress(campaign), 0, 86400)
 
-        logger.info({ campaignId: id }, 'campaign:generate:querying')
-        await generateSendList(campaign)
+            logger.info({ campaignId: id }, 'campaign:generate:querying')
+            await generateSendList(campaign)
 
-        logger.info({ campaignId: id }, 'campaign:generate:sending')
-        await CampaignEnqueueSendsJob.from({
-            id: campaign.id,
-            project_id: campaign.project_id,
-        }).queue()
+            logger.info({ campaignId: id }, 'campaign:generate:sending')
+            await CampaignEnqueueSendsJob.from({
+                id: campaign.id,
+                project_id: campaign.project_id,
+            }).queue()
 
-        await releaseLock(key)
+            await releaseLock(key)
+        } catch (error) {
+            logger.info({ error }, 'campaign:generate:failed')
+            throw error
+        } finally {
+            await releaseLock(key)
+        }
     }
 }
